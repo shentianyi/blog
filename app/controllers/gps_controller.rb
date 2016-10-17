f816f7c0745ce4e0318d1192611955ae1e28295a
Charlot <7wangsong@gmail.com>
2016-10-17 16:19:50 +0800

    req access token and openid

--- app/controllers/gps_controller.rb
+++ app/controllers/gps_controller.rb
@@ -5,5 +5,24 @@ class GpsController<ApplicationController
     @state=params[:state]
 
 
+    uri = URI('https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx41022c4af798f9f5&secret=4bf2030d2a67c34801a8c86c27849854&code='+@code+'&grant_type=authorization_code ')
+    res = Net::HTTP.get_response(uri)
+
+    if res.is_a?(Net::HTTPSuccess)
+      p res.body
+      result=JSON.parse(res.body)
+
+      puts @access_token=result['access_token']
+      puts @openid=result['openid']
+
+    end
+
+
+    render json:{
+               code: @code,
+               state:@state,
+               access_token:@access_token,
+               openid:@openid
+           }
   end
 end
